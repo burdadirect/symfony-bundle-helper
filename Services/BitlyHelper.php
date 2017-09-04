de2c6ee13fcc6c7d2bcbdf6d8d82c531e259c826
Christian Puchinger <puchinger@playboy.de>
2017-09-04 12:04:39 +0200

    Add multiple config domains.

--- Services/BitlyHelper.php
+++ Services/BitlyHelper.php
@@ -10,40 +10,49 @@ class BitlyHelper
   /** @var array */
   private $config;
 
-  /** @var string */
-  private $accessToken;
+  /** @var array */
+  private $accessTokens;
 
   public function __construct($config) {
     $this->config = $config;
   }
 
+  public function getConfig() {
+    return $this->config;
+  }
+
   public function get($endpoint, $params) {
-    $paramsWithToken = array_merge($params, ['access_token' => $this->getAccessToken()]);
+    $paramsWithToken = array_merge($params, ['access_token' => $this->getAccessToken($params['domain'] ?? 'default')]);
 
     return bitly_get($endpoint, $paramsWithToken);
   }
 
   public function post($endpoint, $params) {
-    $paramsWithToken = array_merge($params, ['access_token' => $this->getAccessToken()]);
+    $paramsWithToken = array_merge($params, ['access_token' => $this->getAccessToken($params['domain'] ?? 'default')]);
 
     return bitly_post($endpoint, $paramsWithToken);
   }
 
-  public function getAccessToken() {
-    if ($this->accessToken === NULL) {
+  public function getAccessToken($domain = 'default') {
+    if (!isset($this->accessTokens[$domain])) {
+      $configKey = $domain;
+      if (!isset($this->config[$configKey])) {
+        $configKey = 'default';
+      }
+
       $result = bitly_oauth_access_token_via_password(
-        $this->config['user_login'],
-        $this->config['user_password'],
-        $this->config['client_id'],
-        $this->config['client_secret']
+        $this->config[$configKey]['user_login'] ?? '',
+        $this->config[$configKey]['user_password'] ?? '',
+        $this->config[$configKey]['client_id'] ?? '',
+        $this->config[$configKey]['client_secret'] ?? ''
       );
 
       if (isset($result['access_token'])) {
-        $this->accessToken = $result['access_token'];
+        $this->accessTokens[$domain] = $result['access_token'];
       }
     }
 
-    return $this->accessToken;
+    return $this->accessTokens[$domain];
   }
 
 }
