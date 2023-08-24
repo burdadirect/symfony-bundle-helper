761cab93729a9f2870d604d9c49afb82527ae104
Joachim Krempel <joachim.krempel@burda.com>
2023-08-24 16:43:37 +0200

    Replace @required with attribute

--- src/Traits/ServiceDependencies/HmacHelperDependencyTrait.php
+++ src/Traits/ServiceDependencies/HmacHelperDependencyTrait.php
@@ -3,18 +3,18 @@
 namespace HBM\HelperBundle\Traits\ServiceDependencies;
 
 use HBM\HelperBundle\Service\HmacHelper;
+use Symfony\Contracts\Service\Attribute\Required;
 
 trait HmacHelperDependencyTrait {
 
   protected HmacHelper $hmacHelper;
 
   /**
-   * @required
-   *
    * @param HmacHelper $hmacHelper
    *
    * @return void
    */
+  #[Required]
   public function setHmacHelper(HmacHelper $hmacHelper): void {
     $this->hmacHelper = $hmacHelper;
   }
--- src/Traits/ServiceDependencies/SanitizingHelperDependencyTrait.php
+++ src/Traits/ServiceDependencies/SanitizingHelperDependencyTrait.php
@@ -3,18 +3,18 @@
 namespace HBM\HelperBundle\Traits\ServiceDependencies;
 
 use HBM\HelperBundle\Service\SanitizingHelper;
+use Symfony\Contracts\Service\Attribute\Required;
 
 trait SanitizingHelperDependencyTrait {
 
   protected SanitizingHelper $sanitizingHelper;
 
   /**
-   * @required
-   *
    * @param SanitizingHelper $sanitizingHelper
    *
    * @return void
    */
+  #[Required]
   public function setSanitizingHelper(SanitizingHelper $sanitizingHelper): void {
     $this->sanitizingHelper = $sanitizingHelper;
   }
